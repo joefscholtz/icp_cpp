c3812932c86210f04486abe337f6d4d669d0f844
Joe Ferreira Scholtz <joefscholtz@gmail.com>
2026-03-09 16:40:50 -0300

    add icp settings window

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,43 @@ struct Point3D {
     float x, y, z;
 };
 
+const char* corrMethods[] = { "Naive (Brute Force)", "KD-Tree" };
+static int selectedCorr = 0;
+
+// 2. Minimization Selection
+const char* miniMethods[] = { "Point-to-Point (SVD)", "Point-to-Point (LS)", "Point-to-Plane (LS)", "Generalized ICP" };
+static int selectedMini = 0; 
+
+void ICPSettingsCallback() {
+    ImGui::Begin("ICP Settings");
+
+    ImGui::Text("Pipeline Configuration");
+    ImGui::Separator();
+
+    ImGui::Combo("Correspondence", &selectedCorr, corrMethods, IM_ARRAYSIZE(corrMethods));
+    
+    ImGui::Combo("Minimization", &selectedMini, miniMethods, IM_ARRAYSIZE(miniMethods));
+
+    ImGui::Separator();
+    
+    if (ImGui::Button("Run Iteration")) {
+        if (selectedCorr == 0) {  } 
+        else {  }
+
+        if (selectedMini == 0) {
+            std::cout << "Running Point-to-Point with SVD..." << std::endl;
+        }
+    }
+
+    ImGui::Text("Current RMSE: %.6f", 0.00234);
+    ImGui::End();
+}
+
+
+void mainCallback() {
+    ICPSettingsCallback(); 
+}
+
 int main(int argc, char** argv) {
     polyscope::init();
     polyscope::options::groundPlaneMode = polyscope::GroundPlaneMode::None;
@@ -22,6 +59,10 @@ int main(int argc, char** argv) {
     pc->setPointColor({1.0f, 0.0f, 0.0f}); // RGB
 
     std::cout << "Polyscope initialized. Press 'q' in the window to exit." << std::endl;
+
+    polyscope::options::openImGuiWindowForUserCallback = false;
+
+    polyscope::state::userCallback = mainCallback;
 
     polyscope::show();
 
