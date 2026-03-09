4c6f94a500f1290f69b9ec37094ce51dd540aea7
Joe Ferreira Scholtz <joefscholtz@gmail.com>
2026-03-09 19:50:41 -0300

    change 3rd_party to external

--- core/include/correspondences.hpp
+++ core/include/correspondences.hpp
@@ -1,21 +1,18 @@
 #include <Eigen/Dense>
-#include <vector>
-#include <optional>
 #include <limits>
+#include <optional>
+#include <vector>
 
-using correspondence_t = std::pair<size_t, size_t>; 
+using correspondence_t = std::pair<size_t, size_t>;
 
-std::vector<correspondence_t> correspondence_nn(
-    const std::vector<Eigen::Vector3d>& P,
-    const std::vector<Eigen::Vector3d>& Q,
-    std::optional<double> max_dist)
-{
+std::vector<correspondence_t> correspondence_nn(const std::vector<Eigen::Vector3d> &P, const std::vector<Eigen::Vector3d> &Q,
+                                                std::optional<double> max_dist) {
     std::vector<correspondence_t> correspondences;
 
     // Pre-calculating the squared distance threshold if max_dist is provided
     std::optional<double> max_dist_sq;
-  aaa
-    if (max_dist) max_dist_sq = (*max_dist) * (*max_dist);
+    if (max_dist)
+        max_dist_sq = (*max_dist) * (*max_dist);
 
     for (size_t i = 0; i < P.size(); ++i) {
         double d_min_sq = std::numeric_limits<double>::max();
@@ -23,7 +20,7 @@ std::vector<correspondence_t> correspondence_nn(
         bool found = false;
 
         for (size_t j = 0; j < Q.size(); ++j) {
-            double d2 = (P[i] - Q[j]).squaredNorm(); 
+            double d2 = (P[i] - Q[j]).squaredNorm();
             if (d2 < d_min_sq) {
                 d_min_sq = d2;
                 best_idx = j;
